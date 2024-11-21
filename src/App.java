2903c229c01229bd854c607d9c47b5aa2375451b
fernadev14 <fernandoperez.99a@gmail.com>
2024-11-20 22:27:07 -0500

    Viaje interplanetario java

---
+++ src/App.java
@@ -0,0 +1,21 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[] planetas = {
+                "Mercurio",
+                "Venus",
+                "Marte",
+                "Jupiter",
+                "Saturno"
+        };
+
+        String[] naves = {
+                "Endorex x5050",
+                "InteresTelar",
+                "Enterprise",
+                "Galactica",
+                "Start ship"
+        };
+
+    }
+}
