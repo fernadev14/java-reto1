44377a080ddf8b92fe232a02354635e3d0e9dc37
fernadev14 <fernandoperez.99a@gmail.com>
2024-11-20 23:21:32 -0500

    Array planetas y naves, iteraccion usuario por teclado

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
         String[] planetas = {
                 "Mercurio",
                 "Venus",
@@ -17,5 +20,43 @@ public class App {
                 "Start ship"
         };
 
+        // ELEGIR DESTINO
+        System.out.println("==================================");
+        System.out.println("####### MENU DE PLANETAS ########");
+        System.out.println("\n" + "1. Mercurio" + "\n" +
+                "2. Venus" + "\n" +
+                "3. Marte" + "\n" +
+                "4. Jupiter" + "\n" +
+                "5. Saturno" + "\n");
+
+        System.out.print("Elige el destino planetario donde quieres ir -> ");
+        var eleccionPlaneta = scanner.nextInt();
+
+        if (eleccionPlaneta >= 0 && eleccionPlaneta <= planetas.length) {
+            System.out.printf("Has eligido viajar a: %s%n", planetas[eleccionPlaneta - 1].toUpperCase());
+        } else {
+            System.err.println("Elección incorrecta");
+        }
+
+        // ELEGIR NAVE
+        System.out.println("");
+        System.out.println("==================================");
+        System.out.println("####### MENU DE NAVES ########");
+        System.out.println("\n" + "1. Endorex x5050" + "\n" +
+                "2. InteresTelar" + "\n" +
+                "3. Enterprise" + "\n" +
+                "4. Galactica" + "\n" +
+                "5. Start ship" + "\n");
+
+        System.out.print("Elige el destino planetario donde quieres ir -> ");
+        var eleccionNave = scanner.nextInt();
+
+        if (eleccionNave >= 0 && eleccionNave <= planetas.length) {
+            System.out.printf("Has eligido la nave: %s%n", naves[eleccionNave - 1].toUpperCase());
+        }
+
+        // CALCULAR DISTANCIA Y TIEMPO
+
     }
+
 }
