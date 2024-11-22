ea54ddfe36eccb88626906171fa5d5fbdb7d7504
fernadev14 <fernandoperez.99a@gmail.com>
2024-11-21 22:06:56 -0500

    update bucle while

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -9,7 +10,8 @@ public class App {
                 "Venus",
                 "Marte",
                 "Jupiter",
-                "Saturno"
+                "Saturno",
+                "Urano"
         };
 
         String[] naves = {
@@ -20,6 +22,21 @@ public class App {
                 "Start ship"
         };
 
+        var eleccionPlaneta = -1;
+        var eleccionNave = -1;
+        var nombre = "";
+
+        Arrays.sort(planetas);
+        Arrays.sort(naves);
+
+        // PEDIR NOMBRE AL USUARIO
+        while (nombre.isEmpty()) {
+            System.out.print("Ingresa tu nombre -> ");
+            nombre = scanner.nextLine();
+            nombre = capitalizeFirstLetter(nombre);
+        }
+        System.out.println("");
+
         // ELEGIR DESTINO
         System.out.println("==================================");
         System.out.println("####### MENU DE PLANETAS ########");
@@ -27,33 +44,39 @@ public class App {
             System.out.printf("Planeta %d: %s%n", (i + 1), planetas[i]);
         }
 
-        System.out.print("Elige el destino planetario donde quieres ir -> ");
-        var eleccionPlaneta = scanner.nextInt();
-
-        if (eleccionPlaneta >= 0 && eleccionPlaneta <= planetas.length) {
-            System.out.printf("Has eligido viajar a: %s%n", planetas[eleccionPlaneta - 1].toUpperCase());
-        } else {
-            System.err.println("Elección incorrecta");
+        // PEDIR AL USUARIO ELEGIR SU DESTINO
+        while (eleccionPlaneta <= 0 || eleccionPlaneta > planetas.length) {
+            System.out.printf("%nHola %s, elige el destino planetario donde quieres ir -> ", nombre);
+            eleccionPlaneta = scanner.nextInt();
         }
+        System.out.printf("%nHas eligido viajar a: %s%n%n", planetas[eleccionPlaneta - 1].toUpperCase());
 
         // ELEGIR NAVE
-        System.out.println("");
         System.out.println("==================================");
         System.out.println("####### MENU DE NAVES ########");
         for (int i = 0; i < naves.length; i++) {
             System.out.printf("Nave %d: %s%n", (i + 1), naves[i]);
         }
 
-        System.out.print("Elige la nave para comenzar el viaje -> ");
-        var eleccionNave = scanner.nextInt();
-
-        if (eleccionNave >= 0 && eleccionNave <= planetas.length) {
-            System.out.printf("Has eligido la nave: %s%n", naves[eleccionNave - 1].toUpperCase());
+        while (eleccionNave <= 0 || eleccionNave > naves.length) {
+            System.out.printf("%nHola %s, elige la nave para comenzar el viaje -> ", nombre);
+            eleccionNave = scanner.nextInt();
         }
+        System.out.printf("%nHas eligido la nave: %s%n", naves[eleccionNave - 1].toUpperCase());
+
+        System.out.printf("%n %s! VES PREPARANDOTE PARA EL VIAJE AL ESPACIO!", nombre.toUpperCase());
 
         // CALCULAR DISTANCIA Y TIEMPO
 
         scanner.close();
+    }
+
+    // METODO PARA CONVERTIR LA PRIMERA LETRA EN MAYUSCULA
+    public static String capitalizeFirstLetter(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
     }
 
 }
