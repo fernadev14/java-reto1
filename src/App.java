22ac1834d2a8220bbb0eab1b25a628a39951f878
Juan Pablo Ayala <juanpabloayala48@gmail.com>
2024-11-23 19:18:36 -0500

    Punto 4 eventos aleatorios

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        Random aleatorio = new Random();
 
         String[] planetas = {
                 "Mercurio",
@@ -59,6 +61,20 @@ public class App {
                 3.7,
                 9.8,
                 5.9
+        };
+
+        String[] eventosAleatorios = {
+                "Alerta, daño en la calefacción",
+                "Falla critica, daño en el motor 2",
+                "Peligro, perdida de oxigeno",
+                "Urgente, lluvia de meteoritos"
+        };
+
+        String[][] solucionEventos = {
+                { "Redirección energia del motor 1", "Apagar iluminación" },
+                { "Apagar motor 2", "Reparar motor 2" },
+                { "Reducir el oxigeno al 50%,", "Redireccionar todo el oxigeno a la cabina del capitan" },
+                { "Desviar la nave", "atravesar la lluvia de meteoritos" }
         };
 
         System.out.println("Bienvenido a spaceX, la empresa de viajes espaciales");
@@ -133,6 +149,68 @@ public class App {
         System.out.printf("Para viajar al planeta %s necesitas una cantidad de oxigeno de %.2f KG %n",
                 planetas[eleccionPlaneta - 1],
                 calculoOxigenoRequerido);
+
+        System.out.printf(
+                "%n%n Abrocha tu cintoron despegamos en 5, 4, 3, 2, 1! %n%n");
+
+        System.out.printf(
+                "%n%n Felicitaciones %s el despegue fue exitoso, la nave opera al 100 porciento de capacidad, en caso de algun contratiempo te informaremos %n%n ",
+                nombre);
+
+        // 4. Eventos Aleatorios
+        int eventoIndex = aleatorio.nextInt(eventosAleatorios.length);
+        var evento = eventosAleatorios[eventoIndex];
+
+        System.out.printf("%n%n Atención %s, %s ", nombre, evento);
+        System.out.printf("%n%n Que quieres hacer:");
+
+        String[] opcionSoluciones = solucionEventos[eventoIndex];
+
+        for (int i = 0; i < opcionSoluciones.length; i++) {
+            System.out.printf("%n Solución %d: %s", (i + 1), opcionSoluciones[i]);
+        }
+
+        var opcionSalvavidas = 0;
+        while (opcionSalvavidas < 1 || opcionSalvavidas > opcionSoluciones.length) {
+            System.out.printf("%n Que opción quieres elegir:");
+            opcionSalvavidas = scanner.nextInt();
+        }
+
+        switch (eventoIndex) {
+            case 0: // Este es la opción de Alerta, daño en la calefacción
+                if (opcionSalvavidas == 1) {
+                    System.out.print("Buen trabajo, esta opción consume un poco más de combustible pero no afecta la velocidad");
+                } else if (opcionSalvavidas == 2) {
+                    System.out.print("Excelente trabajo, esta opción es la ideal no sacrifica ni velocidad ni combustible");
+                }
+                break;
+            case 1: // Esta es la opción de Falla critica, daño en el motor 2,
+                if (opcionSalvavidas == 1) {
+                    System.out.print("Esta opción reduce la velocidad del viaje y el tiempo de llegada");
+                } else if (opcionSalvavidas == 2) {
+                    System.out.print("Esta opción es la ideal, salva la integridad de la nave");
+                }
+                break;
+
+            case 2: // Esta es la opción de Peligro, perdida de oxigeno
+                if (opcionSalvavidas == 1) {
+                    System.out.print("Reducir el oxigeno al 50%");
+                } else if (opcionSalvavidas == 2) {
+                    System.out.print("Redireccionar todo el oxigeno a la cabina del capitan");
+                }
+                break; 
+
+            case 3: // Esta es la opción Urgente, lluvia de meteoritos
+                if (opcionSalvavidas == 1) {
+                    System.out.print("Desviar la nave");
+                } else if (opcionSalvavidas == 2) {
+                    System.out.print("atravesar la lluvia de meteoritos");
+                }
+                break; 
+
+            default:
+                break;
+        }
 
         scanner.close();
     }
